d8c099d6a128f04e22521fbf102dfcec3d80a4d7
medvenoemi <99814418+medvenoemi@users.noreply.github.com>
2023-11-15 22:29:24 +0200

    #1 basic android project and #3 splash screen

--- Algorythmics/app/build.gradle.kts
+++ Algorythmics/app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.algorythmics"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.algorythmics"
@@ -33,13 +33,17 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.8.0")
+    implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
---
+++ Algorythmics/app/src/main/java/com/example/algorythmics/activities/SplashActivity.kt
@@ -0,0 +1,68 @@
+package com.example.algorythmics.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import android.view.WindowManager
+import com.example.algorythmics.R
+
+
+class SplashActivity : AppCompatActivity() {
+
+    companion object{
+        const val TAG = "SplashActivity"
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) // 3000 is the delayed time in milliseconds.
+
+
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStar method called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume method called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause method called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop method called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy method called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart method called")
+    }
+
+
+}
