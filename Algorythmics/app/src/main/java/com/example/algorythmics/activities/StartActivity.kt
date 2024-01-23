fafc504b8148d94fb98ef5a9c9289cc8c264e08e
medvenoemi <99814418+medvenoemi@users.noreply.github.com>
2024-01-23 21:49:23 +0200

    #5 Course screen started

--- Algorythmics/app/src/main/java/com/example/algorythmics/activities/StartActivity.kt
+++ Algorythmics/app/src/main/java/com/example/algorythmics/activities/StartActivity.kt
@@ -1,12 +1,22 @@
 package com.example.algorythmics.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.algorythmics.R
 
 class StartActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        val getStartedButton = findViewById<Button>(R.id.getStartedButton)
+
+        getStartedButton.setOnClickListener {
+            val intent = Intent(this, CoursesActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
